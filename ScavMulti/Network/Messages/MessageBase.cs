5be662a68beb491c412e4e431f25e72c056b8e5f
dath <dath@dath.org>
2025-11-16 16:17:12 +0100

    FIX: forgot to update MessageBase Union

--- ScavMulti/Network/Messages/MessageBase.cs
+++ ScavMulti/Network/Messages/MessageBase.cs
@@ -5,9 +5,11 @@ namespace ScavMulti.Network.Messages;
 [Union(0, typeof(Error))]
 [Union(1, typeof(PeerHandshake))]
 [Union(2, typeof(WorldInfo))]
-[Union(3, typeof(ClientConnected))]
-[Union(4, typeof(ClientDisconnected))]
-[Union(5, typeof(ExpieUpdate))]
+[Union(3, typeof(WorldStateRequest))]
+[Union(4, typeof(WorldState))]
+[Union(5, typeof(ClientConnected))]
+[Union(6, typeof(ClientDisconnected))]
+[Union(7, typeof(ExpieUpdate))]
 public abstract record class MessageBase
 {
 	[Key(1000)]
